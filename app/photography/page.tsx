71a603e6ca8a604ca14340917caa5d083cb796f4
Josh Barnett <oh@heyjo.sh>
2024-09-17 15:39:42 -0400

    Removing random rendering of photos

--- app/photography/page.tsx
+++ app/photography/page.tsx
@@ -2,41 +2,53 @@ import type { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
 
+import nyc1 from "@/public/nyc1.jpg";
+import chicago1 from "@/public/chicago1.jpg";
+import chicago2 from "@/public/chicago2.jpg";
+import chicago3 from "@/public/chicago3.jpg";
+import chicago4 from "@/public/chicago4.jpg";
+import chicago5 from "@/public/chicago5.jpg";
+import florida1 from "@/public/florida1.jpg";
+import florida2 from "@/public/florida2.jpg";
+import florida3 from "@/public/florida3.jpg";
+
 export const metadata: Metadata = {
   title: "photography",
   description: "pictures taken by a person named josh",
 };
 
-const pics = [
-  { src: "/chicago1.jpg", alt: "Chicago", width: 2383, height: 3575 },
-  { src: "/chicago2.jpg", alt: "Chicago", width: 3988, height: 2659 },
-  { src: "/chicago3.jpg", alt: "Chicago", width: 3813, height: 5720 },
-  { src: "/chicago4.jpg", alt: "Chicago", width: 4309, height: 2873 },
-  { src: "/chicago5.jpg", alt: "Chicago", width: 3397, height: 5095 },
-  { src: "/florida1.jpg", alt: "Florida", width: 3697, height: 4929 },
-  { src: "/florida2.jpg", alt: "Florida", width: 4000, height: 6000 },
-  { src: "/florida3.jpg", alt: "Florida", width: 5356, height: 3570 },
-  { src: "/nyc1.jpg", alt: "NYC", width: 5922, height: 3947 },
-];
-
 export default function Photography() {
-  const randomPics = pics.sort(() => Math.random() - 0.5);
   return (
     <>
       <main className="row-start-2">
         <ul role="list" className="flex flex-1 flex-col space-y-6 sm:px-16">
-          {randomPics.map((pic) => (
-            <li key={pic.src.toString()}>
-              <Image
-                src={pic.src}
-                alt={pic.alt}
-                width={pic.width}
-                height={pic.height}
-                sizes="100vw"
-                className="w-full h-auto"
-              />
-            </li>
-          ))}
+          <li className="w-full h-auto">
+            <Image src={nyc1} alt={"NYC"} />
+          </li>
+          <li className="w-full h-auto">
+            <Image src={chicago1} alt={"Chicago"} />
+          </li>
+          <li className="w-full h-auto">
+            <Image src={chicago2} alt={"Chicago"} />
+          </li>
+          <li className="w-full h-auto">
+            <Image src={chicago3} alt={"Chicago"} />
+          </li>
+          <li className="w-full h-auto">
+            <Image src={chicago4} alt={"Chicago"} />
+          </li>
+          <li className="w-full h-auto">
+            <Image src={chicago5} alt={"Chicago"} />
+          </li>
+          <li className="w-full h-auto">
+            <Image src={florida1} alt={"Florida"} />
+          </li>
+          <li className="w-full h-auto">
+            <Image src={florida2} alt={"Florida"} />
+          </li>
+          <li className="w-full h-auto">
+            <Image src={florida3} alt={"Florida"} />
+          </li>
         </ul>
       </main>
       <footer className="row-start-3 flex flex-wrap mt-16">
