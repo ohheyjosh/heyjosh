4005f6d20c57b0522bd8e79d89e56127ac5db39f
Josh Barnett <oh@heyjo.sh>
2025-05-27 14:57:40 -0400

    Formatting pass

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
-import { defineConfig } from 'astro/config';
-import solid from '@astrojs/solid-js';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import solid from "@astrojs/solid-js";
+import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   integrations: [solid(), tailwind()],
-  output: 'static',
-  site: 'https://heyjo.sh/',
+  output: "static",
+  site: "https://heyjo.sh/",
 });
