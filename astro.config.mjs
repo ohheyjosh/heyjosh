bfc739ace1c60ac709587967ee27031bacc538bb
Josh Barnett <oh@heyjo.sh>
2025-05-27 14:45:42 -0400

    Add Netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import solid from '@astrojs/solid-js';
 import tailwind from '@astrojs/tailwind';
+import netlify from '@astrojs/netlify';
 
 export default defineConfig({
   integrations: [solid(), tailwind()],
   output: 'static',
-  site: 'https://ohheyjosh.com',
+  site: 'https://heyjo.sh/',
+  adapter: netlify(),
 });
